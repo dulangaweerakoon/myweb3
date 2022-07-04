2126b339d479f1ba708ee5f5886fab38aa199522
fortify-asia <dulanga.weerakoon.lk@gmail.com>
2022-07-04 23:11:35 +0800

    update news and background

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -72,7 +72,6 @@ class Resume extends Component {
               <div className="three columns header-col">
                   <h1><span>Education</span></h1>
               </div>
-
               <div className="nine columns main-col">
                   <div className="row item">
                       <div className="twelve columns">
